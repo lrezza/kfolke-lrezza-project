ca3adcfba71a4792f1befb138e994de3c4286ce2
lrezza <lrezza@kth.se>
2021-04-20 16:58:53 +0200

    Init main.py

---
+++ src/game.py
@@ -0,0 +1,30 @@
+import pygame as pg
+
+### SETTINGS ###
+display_x, display_y = 400, 600                             
+fps = 5
+
+events = []     
+
+def main():
+    global events
+    display, clock = setup()                                
+
+    while True:                                             # Main gameloop
+        events = pg.event.get()                             # Fetch events such as input
+
+        pg.display.update()                                 # Update changes made to display
+        clock.tick(fps)                                     # Tick
+
+def setup():                                                # Setup window etc
+    pg.display.init()
+    pg.event.get()
+    display = pg.display.set_mode((display_x, display_y))
+    dis.fill((150, 150, 150))
+    pg.display.set_caption('Tetris')
+    clock = pg.time.Clock()
+    return display, clock
+
+
+if __name__ == "__main__":
+    main()
